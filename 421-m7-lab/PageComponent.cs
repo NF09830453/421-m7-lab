c150d4973b2c7683e9bfb3f97dab83999025c508
NF09830453 <fyre.lord31@gmail.com>
2023-09-28 18:26:34 -0400

    Initial commit

---
+++ 421-m7-lab/PageComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _421_m7_lab
+{
+    public interface PageComponent: NovelComponent
+    {
+
+    }
+}
