3ecdc77b9e6a98f5035d5442336052b5763a40dc
sung <heechuw@gmail.com>
2023-09-30 11:14:56 -0400

    frame test

--- 421-m7-lab/Program.cs
+++ 421-m7-lab/Program.cs
@@ -7,11 +7,12 @@ Novel book2 = new Novel("To Kill a Mockingbird");
 
 NovelComponentIF page = new Page();
 CompositePageElement col = new Column();
-CompositePageElement fr = new Frame();
+
 
 book1.Add(page);
+
+Console.WriteLine("\n ==== Adding a Column ==== \n");
 book1.Add(col);
-book1.Add(fr);
 ContentElement line = new LineOfText();
 col.Add(line);
 
@@ -22,6 +23,22 @@ line.Add(new Character('N'));
 line.Add(new Character('G'));
 
 Writer w = new Writer(book1);
+Console.WriteLine("Test case 1: Column contains one LineOfText with five Characters \"SWENG\"");
+Console.WriteLine("Test case 1 output: ");
+w.GetNovel().View();
+
+Console.WriteLine("\n ==== Adding a Frame ==== \n");
+CompositePageElement frame = new Frame();
+book1.Add(frame);
+CompositePageElement columnInsideFrame = new Column();
+ContentElement lineOfFrame = new LineOfText();
+frame.Add(columnInsideFrame);
+columnInsideFrame.Add(lineOfFrame);
+lineOfFrame.Add(new Character('4'));
+lineOfFrame.Add(new Character('2'));
+lineOfFrame.Add(new Character('1'));
+Console.WriteLine("Test case 2: Frame contains one Column which contains one LineOfText with three Characters \"421\"");
+Console.WriteLine("Test case 2 output: ");
 w.GetNovel().View();
 
 Console.ReadLine(); // keep console window open
